7003e2c81f4a1114cfe44ff144492c3487ac1379
haya4 <hayashi.yuu@gmail.com>
2020-08-17 22:39:07 +0900

    getDefaultDelimiter

--- src/org/maripo/josm/easypresets/ui/editor/TagEditor.java
+++ src/org/maripo/josm/easypresets/ui/editor/TagEditor.java
@@ -239,6 +239,7 @@ public class TagEditor {
 			ComboMultiSelect item = createEmptyItem();
 			// set delimiters
 			StringBuilder valueString = new StringBuilder();
+			item.delimiter = getDefaultDelimiter();
 			for (String value: inputValues) {
 				if (valueString.length() > 0) {
 					valueString.append(getDefaultDelimiter());
