28b5f2ca8831eb8bdfe4955da159a47c822a23c0
haya4 <hayashi.yuu@gmail.com>
2020-10-18 12:32:57 +0900

    プリセット名のデフォルトを空欄に変更

--- src/org/maripo/josm/easypresets/ui/ExportDialog.java
+++ src/org/maripo/josm/easypresets/ui/ExportDialog.java
@@ -5,12 +5,14 @@ import static org.openstreetmap.josm.tools.I18n.tr;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JFileChooser;
@@ -59,15 +61,83 @@ public class ExportDialog extends ExtendedDialog {
 	}
 
 	List<PresetWrapper> wrappers = new ArrayList<PresetWrapper>();
-	ParameterPanelName presetsNamePane = new ParameterPanelName(tr("Presets name"), tr("Custom Presets name"), "Custom Presets");
-
+	ParameterPanelName presetsNamePane = new ParameterPanelName(tr("Presets name"), "", "Custom Presets");
+	
 	private void initUI() {
+		// headerPane:Box
+		// headerPane:Box:1 <-- presetsNamePane:Flow:320x30
+		final JPanel headerPane = getHeaderPane();
+		
+		// mainPane:Grid
+		// mainPane:Grid:1 <-- JLabel
+		// mainPane:Grid:2 <-- listPane:Grid
+		// mainPane:Grid:3 <-- alertLabel
+		// mainPane:Grid:4 <-- selectAllButton
+		// mainPane:Grid:5 <-- deselectAllButton
+		final JPanel mainPane = getMainPane();
+
+		// buttonPane:Grid
+		// buttonPane:Grid:1 <-- exportButton
+		// buttonPane:Grid:2 <-- cancelButton
+		final JPanel buttonPane = getButtonPanel();
+		
+		// basePane:Box
+		// basePane:Box:1 <-- headerPane
+		// basePane:Box:2 <-- mainPane
+		// basePane:Box:3 <-- buttonPane
+		JPanel basePanel = new JPanel();
+		basePanel.setLayout(new BoxLayout(basePanel, BoxLayout.Y_AXIS));
+		basePanel.add(headerPane);
+		basePanel.add(mainPane);
+		basePanel.add(buttonPane);
+		
+		// ExportDialog:1 <-- basePane
+		setContent(basePanel);
+		
+        SwingUtilities.invokeLater(new Runnable() {
+        	@Override
+            public void run() {
+                toFront();
+            }
+        });
+	}
+	
+	/**
+	 * panel:Box
+	 * panel:Box:1 <-- presetsNamePane:Flow:320x30
+	 * 
+	 * @return panel 
+	 */
+	private JPanel getHeaderPane() {
+		presetsNamePane.setPreferredSize(new Dimension(320,30));
+		String rootName = this.presets.getLocaleName();
+		if (!rootName.equals("Custom Presets")) {
+			presetsNamePane.setText(rootName);
+		}
+
+		JPanel panel = new JPanel();
+		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+		panel.add(presetsNamePane);
+		return panel;
+	}
+	
+	/**
+	 * 
+	 * mainPane:Grid
+	 * mainPane:Grid:1 <-- JLabel
+	 * mainPane:Grid:2 <-- listPane
+	 * mainPane:Grid:3 <-- alertLabel
+	 * mainPane:Grid:4 <-- selectAllButton
+	 * mainPane:Grid:5 <-- deselectAllButton
+	 * 
+	 * @return mainPane
+	 */
+	private JPanel getMainPane() {
+		// mainPane:Grid:2 <-- listPane:Grid
 		JPanel listPane = new JPanel(new GridBagLayout());
-		final JPanel mainPane = new JPanel(new GridBagLayout());
 
-		mainPane.add(presetsNamePane);
-		mainPane.add(new JLabel(tr("Please check presets you want to export.")), GBC.eol().fill());
-
+		// mainPane:Grid:2 <-- listPane:Grid <-- listScroll::320x420
+		// mainPane:Grid:2 <-- listPane:Grid <-- listScroll:1 <-- list:Grid
 		final JPanel list = new JPanel(new GridBagLayout());
 		list.setBackground(Color.WHITE);
 		
@@ -81,34 +151,51 @@ public class ExportDialog extends ExtendedDialog {
 		
 		JScrollPane listScroll = new JScrollPane(list);
 		listScroll.setPreferredSize(new Dimension(320,420));
-		
 		listPane.add(listScroll, GBC.std());
-		mainPane.add(listPane, GBC.eol().fill());
+
+		alertLabel = new JLabel(" ");
+		alertLabel.setForeground(Color.RED);
 		
 		final JButton selectAllButton = new JButton(tr("Check all"));
-		final JButton deselectAllButton = new JButton(tr("Uncheck all"));
 		selectAllButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				selectAll(true);
 			}
 		});
+		
+		final JButton deselectAllButton = new JButton(tr("Uncheck all"));
 		deselectAllButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				selectAll(false);
 			}
 		});
-		
+				
+		final JPanel mainPane = new JPanel(new GridBagLayout());
+		mainPane.add(new JLabel(tr("Please check presets you want to export.")), GBC.eol().fill());
+		mainPane.add(listPane, GBC.eol().fill());
+		mainPane.add(alertLabel, GBC.eol().fill());
+		mainPane.add(selectAllButton, GBC.std());
+		mainPane.add(deselectAllButton, GBC.eol());
+		return mainPane;
+	}
+	
+	/**
+	 * 
+	 * buttonPane:Grid
+	 * buttonPane:Grid:1 <-- exportButton
+	 * buttonPane:Grid:2 <-- cancelButton
+	 * 
+	 * @return buttonPane
+	 */
+	private JPanel getButtonPanel() {
 		final JButton exportButton = new JButton(tr("Export"));
 		exportButton.addActionListener(new ActionListener () {
-
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				exportSelected();
-				
 			}
-			
 		});
 
 		final JButton cancelButton = new JButton(tr("Close"));
@@ -118,24 +205,14 @@ public class ExportDialog extends ExtendedDialog {
 				cancel();
 			}
 		});
-
-		mainPane.add(selectAllButton, GBC.std());
-		mainPane.add(deselectAllButton, GBC.eol());
-		alertLabel = new JLabel(" ");
-		alertLabel.setForeground(Color.RED);
-		mainPane.add(alertLabel, GBC.eol().fill());
-
-		mainPane.add(exportButton, GBC.std());
-		mainPane.add(cancelButton, GBC.eol());
 		
-		setContent(mainPane);
-        SwingUtilities.invokeLater(new Runnable() {
-        	@Override
-            public void run() {
-                toFront();
-            }
-        });
+		final JPanel buttonPane = new JPanel(new GridBagLayout());
+		buttonPane.add(exportButton, GBC.std());
+		buttonPane.add(cancelButton, GBC.eol());
+		return buttonPane;
 	}
+	
+	
 	protected void selectAll(boolean selected) {
 		for (PresetWrapper wrapper: wrappers) {
 			wrapper.checkbox.setSelected(selected);
