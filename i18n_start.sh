9e82eca86d774997dd3346b055f6a5b5f324ddfb
haya4 <hayashi.yuu@gmail.com>
2020-11-01 16:16:12 +0900

    Transport langage

--- i18n_start.sh
+++ i18n_start.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
+cd src
 find . -name '*.java' | xargs xgettext -k -ktrc:1c,2 -kmarktrc:1c,2 -ktr -kmarktr -ktrn:1,2 -ktrnc:1c,2,3
-mv -f messages.po i18n
+mv -f messages.po ../i18n
+cd ..
 # Backup
 cd i18n
 mv -f ja.po ja.po.prev
