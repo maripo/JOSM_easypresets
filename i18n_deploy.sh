e6dbfa6d545a023270dd31405a29ba424d8f14c2
Mariko Hisakawa <avenafatua@gmail.com>
2021-02-07 23:30:44 +0900

    Add French to i18n build script

--- i18n_deploy.sh
+++ i18n_deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-perl ../../i18n/i18n.pl --basedir=data --potfile=i18n/messages.po i18n/en.po i18n/ja.po i18n/hu.po
+perl ../../i18n/i18n.pl --basedir=data --potfile=i18n/messages.po i18n/en.po i18n/ja.po i18n/hu.po i18n/fr.po
 ant clean
 
